ed140234c68eb6c0f5f3586c908b3753915305db
Rafael Martin Alves Ferreira <117859338+rafaferreira011@users.noreply.github.com>
2023-05-27 19:33:52 -0300

    Update main.tf

--- descomplicando-terraform/main.tf
+++ descomplicando-terraform/main.tf
@@ -1,17 +1,16 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-#provider "aws" {
-#  region = "us-east-1"
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 4.0"
+#    }
+#  }
 #}
 
+provider "aws" {
+  version = "~> 4.0"
+  region  = "us-east-1"
+}
 
 #terraform {
  # backend "s3" {
