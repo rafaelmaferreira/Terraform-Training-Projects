eacfa698644e7a34868b7ce6f182301f59328660
Rafael Martin Alves Ferreira <117859338+rafaferreira011@users.noreply.github.com>
2023-05-27 19:30:47 -0300

    Update main.tf

--- descomplicando-terraform/main.tf
+++ descomplicando-terraform/main.tf
@@ -1,7 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  version = "~>3.0"
-  #region  = "${terraform.workspace == "prd" ? "us-east-1" : 1}"
 }
 
 
