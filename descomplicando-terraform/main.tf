31af8f0f646428cd70a4da62d047b6a6cc84fb7b
Rafael Martin Alves Ferreira <117859338+rafaferreira011@users.noreply.github.com>
2023-05-27 19:52:21 -0300

    Update main.tf

--- descomplicando-terraform/main.tf
+++ descomplicando-terraform/main.tf
@@ -11,8 +11,8 @@
 provider "aws" {
   region  = "us-east-1"
   version = "~> 4.0"
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_ACCESS_KEY_KEY"
+  access_key = "AKIA6LA7FIKNOUUTEWPB"
+  secret_key = "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"
 }
 
 #terraform {
