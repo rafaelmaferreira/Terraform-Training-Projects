7a4ae894547ce4d4a963238f405a2d91e234d560
Rafael Martin Alves Ferreira <117859338+rafaferreira011@users.noreply.github.com>
2023-06-02 09:03:52 -0300

    Add files via upload

--- descomplicando-terraform/aulas/variables.tf
+++ descomplicando-terraform/aulas/variables.tf
@@ -4,8 +4,43 @@ variable "image_id" {
   description = "The AMI to use for the EC2 instance"
 }
 
+variable "instance_type" {
+  default     = "t3.micro"
+  type        = string
+  description = "The type of EC2 instance to launch"
+}
+
 variable "name" {
   type = string
-  default ="gomex"
-  description = "nome do "
+  default = "sem nome"
+  description = "nome"
+}
+
+variable "projeto" {
+  description = "mapa de projetos"
+  type = map
+  default = {
+    projeto1 = {
+      instance_type = "t2.micro"
+      app_name      = "projeto1"
+    },
+    projeto2 = {
+      instance_type = "t2.micro"
+      app_name      = "projeto2"
+    },
+  }
+  
+}
+
+variable "nome" {
+  type = string
+
+  validation {
+    condition = (
+      length(var.am_id) > 4 &&
+      substr(var.ami_id, 0, 4) == "ami-"
+    )
+    error_message = "the ami_id value must start with ami-"
+  }
+  
 }
